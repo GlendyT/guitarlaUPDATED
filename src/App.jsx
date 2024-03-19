ed81e56dea92ab6b3075d4bb01a9021d7246b10b
GlendyT <glendytuyuc@gmail.com>
2024-03-19 15:27:13 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+
+import { useState } from "react"
+import Guitar from "./components/Guitar"
+import Header from "./components/Header"
+import { db } from "./data/db"
+
+
+function App() {
+  const [ data, setData] = useState(db)
+  const [ cart, setCart ] = useState([])
+
+  function addToCart(item) {
+    setCart(prevCart => [...prevCart, item])
+  }
+
+  //forma correcta si hay una API
+ // useEffect(() => {
+ //   setData(db)
+ // }, [])
+
+  
+  return (
+    <>
+    <Header/>
+
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+             <Guitar
+              guitar={guitar}
+              key={guitar.id}
+              setCart={setCart}
+              addToCart={addToCart}
+             />
+          ))}
+
+           
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+
+    </>
+  )
+}
+
+export default App
